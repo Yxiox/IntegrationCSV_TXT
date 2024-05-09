7c999ab6f74532c359dc5d4d70bda0173fe8919e
Mateus788 <97473017+Mateus788@users.noreply.github.com>
2024-05-08 21:36:34 -0300

    backup

--- IntegrationPlataform/Core/Csv/CsvImporter.cs
+++ IntegrationPlataform/Core/Csv/CsvImporter.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CsvHelper.Configuration;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+using System.Globalization;
+using CsvHelper;
+using Core.Data;
 
 namespace Core.Csv
 {
-    internal class CsvImporter
+    public class CsvImporter
     {
+        public void Import()
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true
+                //HeaderValidated = null
+            };
+
+            using (var reader = new StreamReader(@"C:\\Users\\mateus.oliveira7\\Downloads\\testeTXT2.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.CurrentCulture))
+            {
+                var midias = csv.GetRecords<CsvRepository>();
+                foreach (var midia in midias)
+                {
+                    Console.WriteLine(midia.GENERO);
+                }
+            }
+        }
     }
 }
---
+++ IntegrationPlataform/Core/Csv/CsvMap.cs
@@ -0,0 +1,18 @@
+﻿using Core.Data;
+using CsvHelper.Configuration;
+
+namespace Core.Csv
+{
+    public class CsvMap : ClassMap<datamodel>
+    {
+        public CsvMap()
+        {
+            Map(m => m.GENERO).Name("GENERO");
+            Map(m => m.CATEGORIA).Name("CATEGORIA");
+            Map(m => m.MIDIA).Name("MIDIA");
+            Map(m => m.TIPO_MIDIA).Name("TIPO_MIDIA");
+            Map(m => m.CLASSIFICACAO).Name("CLASSIFICACAO");
+            Map(m => m.PARTICIPANTE).Name("PARTICIPANTE");
+        }
+    }
+}
