86f9dfc3d9abe7ffef5529e6763944902ef5d239
Yxiox <bruno.dela.goncalves@gmail.com>
2024-05-14 21:39:05 -0300

    config restante

--- IntegrationPlataform/Integration.API/Controllers/CategoriaController.cs
+++ IntegrationPlataform/Integration.API/Controllers/CategoriaController.cs
@@ -1,7 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Integration.API.Integration.API.Entities;
+using Integration.API.Integration.API.Repositories;
+using Integration.API.Integration.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace Integration.API.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class CategoriaController : ControllerBase { }
+public class CategoriaController : ControllerBase
+{
+    private readonly CATEGORIA_REPOSITORY _categoria_repository;
+
+    public CategoriaController(IDbConnection conn)
+    {
+        _categoria_repository = new(conn);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> InserirAsync(CATEGORIA categoria)
+    {
+        var result = await _categoria_repository.InsertAsync(categoria);
+
+        if (result > 0) return Ok();
+
+        return BadRequest(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        return Ok(await _categoria_repository.GetAllAsync());
+    }
+}
--- IntegrationPlataform/Integration.API/Program.cs
+++ IntegrationPlataform/Integration.API/Program.cs
@@ -1,3 +1,6 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IDbConnection>(x => new MySqlConnection("server=localhost;database=apoionegocio;uid=root;pwd=root"));
 
 var app = builder.Build();
 
