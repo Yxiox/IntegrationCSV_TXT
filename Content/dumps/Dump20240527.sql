a0c4508a64c99d758b8bcdebe56782539f3ac97c
Victor Santos <110687512+VictorSantos09@users.noreply.github.com>
2024-05-27 20:17:15 -0300

    Update Dump20240527.sql

--- Content/dumps/Dump20240527.sql
+++ Content/dumps/Dump20240527.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `apoionegocio` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `apoionegocio`;
 -- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
 -- Host: localhost    Database: apoionegocio
@@ -196,7 +198,9 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-27 20:11:04
+-- Dump completed on 2024-05-27 20:16:32
+CREATE DATABASE  IF NOT EXISTS `legado` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `legado`;
 -- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
 -- Host: localhost    Database: legado
@@ -279,4 +283,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-27 20:11:04
+-- Dump completed on 2024-05-27 20:16:32
